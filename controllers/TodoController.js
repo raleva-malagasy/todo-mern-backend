96feefd627c29eedabcdcd71bf3b988f5105947c
raleva-malagasy <141238377+raleva-malagasy@users.noreply.github.com>
2024-03-27 20:30:49 +0300

    Add files via upload

---
+++ controllers/TodoController.js
@@ -0,0 +1,34 @@
+const TodoModel = require('../models/TodoModel')
+
+module.exports.getTodo = async (req,res) => {
+    const toDo = await TodoModel.find()
+    res.send(toDo)
+}
+
+module.exports.saveTodo = async (req, res) => {
+    const { text } = req.body
+
+    TodoModel
+        .create({text})
+        .then((data) => {
+            console.log('Added Successfully...');
+            console.log(data);
+            res.send(data)
+        })
+}
+
+module.exports.updateTodo = async (req, res) => {
+    const {_id, text} = req.body
+    TodoModel
+        .findByIdAndUpdate(_id, {text})
+        .then(() => res.send('Updated successfully...'))
+        .catch((err) => console.log(err))
+}
+
+module.exports.deleteTodo = async (req, res) => {
+    const {_id} = req.body
+    TodoModel
+        .findByIdAndDelete(_id)
+        .then(() => res.send('Deleted successfully...'))
+        .catch((err) => console.log(err))
+}
